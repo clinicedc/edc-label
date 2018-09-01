fc97290cb61c5c8721242317287a5d56887fb818
erikvw <ew2789@gmail.com>
2018-09-01 11:53:36 -0500

    add bootstrap to path

--- edc_label/templatetags/edc_label_extras.py
+++ edc_label/templatetags/edc_label_extras.py
@@ -1,10 +1,13 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
-@register.inclusion_tag('edc_label/print_button.html')
-def print_button(button_label=None, printer=None, label_template_name=None, **kwargs):
+@register.inclusion_tag(f'edc_label/bootstrap{settings.EDC_BOOTSTRAP}/'
+                        'print_button.html')
+def print_button(button_label=None, printer=None, label_template_name=None,
+                 **kwargs):
     return dict(
         button_label=button_label,
         label_template_name=label_template_name,
@@ -12,7 +15,8 @@ def print_button(button_label=None, printer=None, label_template_name=None, **kw
     )
 
 
-@register.inclusion_tag('edc_label/printer_config.html')
+@register.inclusion_tag(f'edc_label/bootstrap{settings.EDC_BOOTSTRAP}/'
+                        'printer_config.html')
 def printer_config(heading=None, printer_type=None, selected_printer=None,
                    printers=None, **kwargs):
 
--- edc_label/views/home_view.py
+++ edc_label/views/home_view.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.views.generic.base import TemplateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_navbar import NavbarViewMixin
@@ -7,6 +8,6 @@ from ..view_mixins import EdcLabelViewMixin
 
 class HomeView(EdcBaseViewMixin, NavbarViewMixin, EdcLabelViewMixin, TemplateView):
 
-    template_name = 'edc_label/home.html'
+    template_name = f'edc_label/bootstrap{settings.EDC_BOOTSTRAP}/home.html'
     navbar_name = 'edc_label'
     navbar_selected_item = 'label'
