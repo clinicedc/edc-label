a8371d85cf0d607c640f24abf25cfd0614574143
erikvw <ew2789@gmail.com>
2021-10-12 20:46:39 -0500

    window history back settings attr

--- edc_label/views/browser_print_labels_view.py
+++ edc_label/views/browser_print_labels_view.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.views.generic import TemplateView
 from edc_dashboard.view_mixins import EdcViewMixin
 from edc_navbar import NavbarViewMixin
@@ -11,4 +12,8 @@ class BrowserPrintLabelsView(EdcViewMixin, NavbarViewMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        browser_print_page_auto_back = getattr(
+            settings, "EDC_LABEL_BROWSER_PRINT_PAGE_AUTO_BACK", True
+        )
+        context.update(browser_print_page_auto_back=browser_print_page_auto_back)
         return context
