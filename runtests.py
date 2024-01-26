85b1494b0584c1408defd373a4880628e9b09acb
erikvw <ew2789@gmail.com>
2024-01-26 08:36:33 -0600

    fix tests

--- runtests.py
+++ runtests.py
@@ -7,7 +7,6 @@ from os.path import abspath, dirname
 import django
 from django.conf import settings
 from django.test.runner import DiscoverRunner
-from edc_constants.constants import IGNORE
 from edc_test_utils import DefaultTestSettings
 
 app_name = "edc_label"
@@ -18,8 +17,15 @@ DEFAULT_SETTINGS = DefaultTestSettings(
     BASE_DIR=base_dir,
     APP_NAME=app_name,
     ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
-    SUBJECT_VISIT_MODEL="edc_lab.subjectvisit",
-    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    SILENCED_SYSTEM_CHECKS=[
+        "sites.E101",
+        "edc_navbar.E002",
+        "edc_navbar.E003",
+        "edc_consent.E001",
+        "edc_sites.E001",
+        "edc_sites.E002",
+    ],
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
     EDC_AUTH_SKIP_SITE_AUTHS=True,
     EDC_AUTH_SKIP_AUTH_UPDATER=True,
     INSTALLED_APPS=[
@@ -31,11 +37,21 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "django.contrib.staticfiles",
         "django.contrib.sites",
         "django_crypto_fields.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
         "edc_auth.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
         "edc_identifier.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
         "edc_notification.apps.AppConfig",
         "edc_sites.apps.AppConfig",
         "edc_label.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
     ],
     add_dashboard_middleware=True,
 ).settings
